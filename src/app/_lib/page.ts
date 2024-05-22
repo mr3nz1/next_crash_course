a2eeef8c59d00e6e3b4b36753ecc0ef3f19f2d29
Murenzi Paterne <murenzi419@gmail.com>
2024-05-22 13:35:25 +0200

    ft

--- src/app/_lib/page.ts
+++ src/app/_lib/page.ts
@@ -3,7 +3,7 @@ import React from "react";
 export default function PrivateRoute() {
   return (
     <div>
-      <h1>You can not view this in the browser</h1>
+      <h1>You can not view  this in the browser</h1>
     </div>
   );
 }
